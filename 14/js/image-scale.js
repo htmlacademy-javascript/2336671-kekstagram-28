298fd30edde3caae45f8ac8e428d7f08c76411f4
Keks <keks@htmlacademy.ru>
2023-03-28 12:09:21 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/image-scale.js
@@ -0,0 +1,42 @@
+const imageUploadPreview = document.querySelector('.img-upload__preview img');
+const scaleControlSmaller = document.querySelector('.scale__control--smaller');
+const scaleControlBigger = document.querySelector('.scale__control--bigger');
+const scaleControlValue = document.querySelector('.scale__control--value');
+
+const SCALE_STEP = 25;
+const MIN_SCALE = 25;
+const MAX_SCALE = 100;
+
+const setImageScale = (scaleValue) => {
+  imageUploadPreview.style.setProperty('transform', `scale(${scaleValue / 100})`);
+  scaleControlValue.setAttribute('value', `${scaleValue}%`);
+};
+
+const onMinusClick = () => {
+  let scaleValue = parseInt(scaleControlValue.value, 10);
+  scaleValue -= SCALE_STEP;
+
+  if (scaleValue < MIN_SCALE) {
+    scaleValue = MIN_SCALE;
+  }
+
+  scaleControlValue.value = `${scaleValue}%`;
+  setImageScale(scaleValue);
+};
+
+const onPlusClick = () => {
+  let scaleValue = parseInt(scaleControlValue.value, 10);
+  scaleValue += SCALE_STEP;
+
+  if (scaleValue > MAX_SCALE) {
+    scaleValue = MAX_SCALE;
+  }
+
+  scaleControlValue.value = `${scaleValue}%`;
+  setImageScale(scaleValue);
+};
+
+scaleControlSmaller.addEventListener('click', onMinusClick);
+scaleControlBigger.addEventListener('click', onPlusClick);
+
+export {setImageScale};
---
+++ 14/js/utils.js
@@ -0,0 +1,47 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const createRandomIdFromRangeGenerator = (min, max) => {
+  const previousValues = [];
+
+  return () => {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '14px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, 5000);
+};
+
+const isEscape = (evt) => evt.key === 'Escape';
+
+export { getRandomInteger, createRandomIdFromRangeGenerator, showAlert, isEscape };
